34065f50bef7b45f90c512c15319fe19e1b0ee22
joshGilsatrp <jgilstrap23@gmail.com>
2023-06-24 09:29:20 -0700

    Added looping ability to change bot mid-run, memory persists. Put limits on tokens; adjust up for more considerations, down for less. Saves money on OpenAI

--- main.py
+++ main.py
@@ -48,39 +48,45 @@ math_tools = load_tools(["serpapi", "llm-math", "wolfram-alpha"], llm=llm)
 memory = ConversationBufferMemory(memory_key="chat_history")
 
 # Plan-and-Execute Model: Use this for research like programming and math
-plan_agent = initialize_agent(plan_tools, llm, AgentType.ZERO_SHOT_REACT_DESCRIPTION, memory=memory, verbose=True)
+plan_agent = initialize_agent(plan_tools, llm, AgentType.ZERO_SHOT_REACT_DESCRIPTION, memory=memory, verbose=True, max_execution_time=5)
 
 # Conversation Modal: Use this for a conversational bot
-convo_agent = initialize_agent(convo_tools, llm, AgentType.CONVERSATIONAL_REACT_DESCRIPTION, memory=memory, verbose=True)
+convo_agent = initialize_agent(convo_tools, llm, AgentType.CONVERSATIONAL_REACT_DESCRIPTION, memory=memory, verbose=True, max_execution_time=5)
 
-# Search Model: Use this for asking questions
-math_agent = initialize_agent(math_tools, llm, AgentType.CONVERSATIONAL_REACT_DESCRIPTION, memory=memory, verbose=True)
+# Math Model: Use this for asking questions
+math_agent = initialize_agent(math_tools, llm, AgentType.CONVERSATIONAL_REACT_DESCRIPTION, memory=memory, verbose=True, max_execution_time=5)
 
 
-# Conversational loop
+# Main loop
 user_input = ''
-print("Would you like to:")
-print("(1) Chat") # convo_agent
-print("(2) Research") # plan_agent
-print("(3) Math") # math_agent
-user_input = input('\nInput choice: ')
-agent = convo_agent
-prompt = "INTERACT ('quit' to exit): "
-if user_input == '1':
-    print("Use this AI to conversate with. Ask questions and make statements in the interest of connection!")
-elif user_input == '2':
-    print("Use this AI for QUESTIONS ONLY. It doesn't like to connect with people...")
-    print("It'll use things like Google, Wikipedia and YouTube to answer your questions succinctly.")
-    agent = plan_agent
-elif user_input == '3':
-    print("Use this AI as a conversational math buddy. It uses all built-in math functions, Google and WolframAlpha for math decisions.")
-    agent = math_agent
-while user_input != 'quit':
-    user_input = input('\n'+prompt)
-    if user_input == 'quit': continue
-    print("---------AI PROCESS BEGIN---------\b")
-    output = agent.run(user_input)
-    print("---------AI PROCESS END---------\n")
-    print(output)
+while user_input != "exit":
+    print("Choose your experience:")
+    print("(1) Chat AI") # convo_agent
+    print("(2) Research AI") # plan_agent
+    print("(3) Math AI") # math_agent
+    print("'exit' to turn off")
+    user_input = input('AI choice: ')
+    agent = convo_agent
+    prompt = "INTERACT ('back' to menu): "
+    if user_input == '1':
+        print("Use this AI to conversate with. Ask questions and make statements in the interest of connection!")
+    elif user_input == '2':
+        print("Use this AI for QUESTIONS ONLY. It doesn't like to connect with people...")
+        print("It'll use things like Google, Wikipedia and YouTube to answer your questions succinctly.")
+        agent = plan_agent
+    elif user_input == '3':
+        print("Use this AI as a conversational math buddy. It uses all built-in math functions, Google and WolframAlpha for math decisions.")
+        agent = math_agent
+    elif user_input == 'exit':
+        exit()
+    # Inner, agent specific conversation loop
+    while user_input != 'back':
+        user_input = input('\n'+prompt)
+        if user_input == 'back': continue
+        print("---------AI PROCESS BEGIN---------\b")
+        output = agent.run(user_input)
+        print("---------AI PROCESS END---------\n")
+        print(user_input)
+        print(output)
     
     
